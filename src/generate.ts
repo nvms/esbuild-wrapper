a94a856d6d062233bf92e5e227f1ee9cc19084ff
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-07 18:15:06 -0700

    Create `public` when tw is chosen

--- src/generate.ts
+++ src/generate.ts
@@ -264,6 +264,23 @@ export async function generate() {
       "postcss-minify": "^1.1.0",
       "tailwindcss": "^3.2.7"
     };
+
+    mkdirSync("public");
+    mkdirSync("public/css");
+    writeFileSync("public/index.html", `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="public/css/main.css" />
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
+`);
   }
 
 
