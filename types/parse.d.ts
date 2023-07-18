37505050c268a0338c56bf24a94e771806b3d058
nvms <jon@pye.rs>
2023-07-18 12:42:49 -0400

    Push lib and types

--- types/parse.d.ts
+++ types/parse.d.ts
@@ -1,3 +1,9 @@
 import { Mode } from "./general.js";
 import { ESBWConfig } from "./interface.js";
+/**
+ * Parses the ESBWConfig file and returns the configuration object.
+ * @param mode The mode to validate the config against.
+ * @returns The parsed ESBWConfig object.
+ * @throws An error if the ESBWConfig file is not found or if the config fails validation.
+ */
 export declare function parseConfig(mode: Mode): Promise<ESBWConfig>;
--- types/stripper.d.ts
+++ types/stripper.d.ts
@@ -4,7 +4,7 @@ import { StagedBuildOptions } from "./interface.js";
  * stripHooks removes configuration properties that
  * don't fulfill esbuild's BuildOptions interface.
  */
-export declare function stripHooks(opts: StagedBuildOptions & {
+export declare function stripHooks({ beforeAll, afterAll, build, runfile, watchPaths, index, injectArtifacts, port, reloadPort, ...rest }: StagedBuildOptions & {
     index?: string;
     port?: number;
     reloadPort?: number;
